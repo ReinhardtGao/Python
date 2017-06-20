c4450ab014b1ca653e4acb969323e2b35b715274
ReinhardtGao <dragongj002@126.com>
2017-06-20 15:30:36 +0800

    chapter 5

--- five.py
+++ five.py
@@ -10,3 +10,13 @@ from zoo import hours as info
 
 info()
 ##################################
+plain = {'a':1, 'b':2, 'c':3}
+print(plain.items())
+from collections import OrderedDict
+fancy = OrderedDict([('a',1),('b',2),('c',3)])
+print(fancy.items())
+###################################
+from collections import defaultdict
+dict_of_lists = defaultdict(list)
+dict_of_lists['a'].append('Something for a')
+print(dict_of_lists['a'])
